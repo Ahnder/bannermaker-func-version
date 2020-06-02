c9ce3d8e7fdca4e724d3476baeb855deedd689f7
Ahnder <skguss1@gmail.com>
2020-06-02 15:30:02 +0900

    palette init setting, Add css files

--- src/components/PaletteComponent.js
+++ src/components/PaletteComponent.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './PaletteComponent.css';
+
+import { CirclePicker, HuePicker } from 'react-color';
 
 const PaletteComponent = () => {
   return (
-    <div>
-      <span>PaletteComponent</span>
+    <div className="paletteWrapper">
+      <div className="circlePicker">
+        <CirclePicker />
+      </div>
+      <div className="huePicker">
+        <HuePicker />
+      </div>
     </div>
   );
 };
